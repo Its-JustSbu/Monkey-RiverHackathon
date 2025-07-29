fc1d68a546926d78f71e0dc207ebf2cabd718245
Its-JustSbu <116superstm@gmail.com>
2025-07-29 10:59:23 +0200

    init-api

---
+++ Backend/Backend/Model/AppContext.cs
@@ -0,0 +1,15 @@
+﻿using Backend.Model.Domain;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Model
+{
+    public class AppContext(DbContextOptions contextOptions) : IdentityDbContext<User>(contextOptions)
+    {
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
